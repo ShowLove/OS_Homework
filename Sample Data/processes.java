6012195e1720d81cb94ff632f5cd8a5e5c6656a2
Carlos Garzon <showlove@knights.ucf.edu>
2016-02-16 00:08:23 -0500

    stored times correctly to sort later

--- Sample Data/processes.java
+++ Sample Data/processes.java
@@ -11,6 +11,46 @@ public class processes
 		processes();
 	}
 
+/*
+            System.out.println("p_"+processCount+" r_"+runFor+" u_"+use);
+            System.out.println(" q_"+quantum+" proCount_"+processCount);
+            System.out.println(" arrival_"+arrival+" burst_"+burst);
+            System.out.println(Arrays.deepToString(arivalBurst));
+*/
+
+	public static void runfcfs(int runFor, int processCount)
+	{
+		int timeData = processCount*3;
+		int arrival = 0;	int burst = 0;
+		int selected = 0;
+		int finished = 0;
+		int prevSelected = 0;
+		int prevFinished = 0;
+
+		//Hashtable<Integer,Integer> timeData = new Hashtable<Integer,Integer>()
+		Integer[][] timeList = new Integer[processCount+1][3];
+
+		//What can happen when i = runFor?
+		for( int p = 1; p <= processCount; p++)
+		{
+			// for p
+			//arrival = arivalBurst[p][0];
+			timeList[p][0] = arivalBurst[p][0];
+			burst = arivalBurst[p][1];
+
+			//selected = prevFinished;
+			timeList[p][1] = prevFinished;
+
+			//finished = prevFinished + burst;
+			timeList[p][2] = prevFinished + burst;
+			System.out.println("Time "+timeList[p][0]+": P"+p+" arrived");
+			System.out.println("Time "+timeList[p][1]+": P"+p+" selected (burst "+burst+")" );
+			System.out.println("Time "+timeList[p][2]+": P"+p+" finished");
+			prevFinished = timeList[p][2];
+		}
+
+	}
+
 	public static void processes()
 	{
        try 
@@ -84,6 +124,7 @@ public class processes
             if(use.equals("fcfs"))
             {
             	System.out.println("use fcfs!!!");
+            	runfcfs(runFor, processCount);
             }
 
         } 
