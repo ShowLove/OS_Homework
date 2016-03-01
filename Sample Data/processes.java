5b87f04014ab2e57e963e3319b376a6e51ee8a1e
Carlos Garzon <showlove@knights.ucf.edu>
2016-03-01 12:31:44 -0500

    finished sjf, need to bulletproof it later if i have time

--- Sample Data/processes.java
+++ Sample Data/processes.java
@@ -9,7 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 ////////////////////////////////////////////////////////////
-/*METHODS
+/* METHODS
 javap processes
 
   public static int[][] arivalBurst;
@@ -164,108 +164,6 @@ public class processes
 
 				}//END OF ARRIVED PROCESS
                 
-                /*  		DEBUG
-                if(t == 8)
-                {
-
-                	debug_pSelected( t, 1, currentBurstT, arivedSortedT, burstSortedT,firstSelected, finished, prevSelectP, prevSelectT, processCount);
-                	debug_pSelected( t, 2, currentBurstT, arivedSortedT, burstSortedT,firstSelected, finished, prevSelectP, prevSelectT, processCount);
-                	debug_pSelected( t, 3, currentBurstT, arivedSortedT, burstSortedT,firstSelected, finished, prevSelectP, prevSelectT, processCount);
-                	debug_pSelected( t, 4, currentBurstT, arivedSortedT, burstSortedT,firstSelected, finished, prevSelectP, prevSelectT, processCount);
-
-                }                                        																	
-				*/
-/*
-				//Check if all Ps arrived And at least one still has burst
-				//If so sequentially print processes with shortest burst untill all are finished
-				int greatestBurst = greatestP_BFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-				//System.out.println("We bout to do something! sb_"+greatestBurst); //DEBUG
-				if(allPsHaveArrived(arrivedProcesses, processCount) && (greatestBurst > 0) && (ranAllPsArrivedIf == false) )
-				{
-					ranAllPsArrivedIf = true;
-
-					//System.out.println("firstArrived_"+Arrays.deepToString(firstSelected) );	DEBUG
-
-					while(greatestBurst > 0)
-					{
-						if(finishedPreviousSelected[0] == 1)
-						{
-							greatestBurst = greatestP_BFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-
-							//System.out.println("We bout to do something! sb_"+greatestBurst); //DEBUG
-							int shortestBurst = shortestP_BFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-							process = greatestB_PFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-
-							currentBurstT = burstTimeForP(burstSortedT, process, processCount);
-
-							//newBurstT = burstTimeForP(myArray, process, processCount) - (t - prevSelectT); DEBUG
-							//prevSelectT is messed up
-							printSelected(bw, t, process, currentBurstT);
-							//keep track of first selected processes
-							insertFirstSelectedData(firstSelected, process, t, processCount);
-
-							updateBTimeToZero(burstSortedT, process, processCount);
-
-							printFinished(bw, t+currentBurstT, process);
-							insertFinishedData(finished, process, t+currentBurstT, processCount);
-							t = t+currentBurstT;
-
-							greatestBurst = greatestP_BFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-						}
-						if(finishedPreviousSelected[0] == 0)
-						{
-							process = shortestB_PFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-
-							currentBurstT = burstTimeForP(burstSortedT, process, processCount);
-
-							printFinished(bw, t+currentBurstT, process);
-							insertFinishedData(finished, process, t+currentBurstT, processCount);
-
-							updateBTimeToZero(burstSortedT, process, processCount);
-
-							t = t+currentBurstT;
-
-							greatestBurst = greatestP_BFromArrivedPs(arrivedProcesses, burstSortedT, processCount);
-						}					
-					}
-
-					while(t < runFor )
-					{
-						bw.write("Time "+t+": IDLE\n");
-						t++;
-					}
-
-					bw.write("Finished at time "+runFor+"\n\n");
-
-					bubbleSortP(firstSelected);
-					bubbleSortP(arivedSortedT);
-					bubbleSortP(finished);
-
-
-					//Print sorted burst/arrival times 	DEBUG
-					//System.out.println("Selected:"+Arrays.deepToString(firstSelected));
-					//System.out.println("finished:"+Arrays.deepToString(finished));
-					//System.out.println("Burst:"+Arrays.deepToString(arivalBurst));
-					//System.out.println("Arrived:"+Arrays.deepToString(arivedSortedT));
-
-					//Print wait and turnaround times
-					//Wait = finish time - arrival - burst. Turnaround = wait + burst
-					for(int i = 0; i < arivedSortedT[0].length; i++)
-					{
-						int p = i;
-						//System.out.println("P_"+(p+1)+" B_"+arivalBurst[p+1][1]+" f_"+finished[1][p]+" A_"+arivedSortedT[1][p]);  // DEBUG
-						int wait = finished[1][p] - arivedSortedT[1][p] - arivalBurst[p+1][1];
-						int turnaround = wait + arivalBurst[p+1][1];
-						printWaitTurnaroundT(bw, p+1, wait, turnaround);
-					}						
-
-					//Print sorted burst/arrival times 	DEBUG
-					//System.out.println("Recursive _t"+t+" p_"+process);
-					//System.out.println("Burst:"+Arrays.deepToString(burstSortedT));
-					//System.out.println("Arrived:"+Arrays.deepToString(arivedSortedT));
-
-				}//END OF ALL P'S HAVE ARRIVED IF
-*/
 
 			bubbleSortFinal(burstSortedT);
 			bubbleSortFinal(arivedSortedT);
@@ -364,13 +262,13 @@ public class processes
 						if(process != 0)
 						{
 							if( !isPFinished(finished, process, processCount) )
-							{	System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+							{	//System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 								currentBurstT = burstTimeForP(burstSortedT, process, processCount);
 								printFinished(bw, prevSelectT+currentBurstT, process);
 								insertFinishedData(finished, process, t+currentBurstT, processCount);
 								updateBTimeToZero(burstSortedT, process, processCount);
 								t = prevSelectT+currentBurstT;  //t++ is why -1
-								System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+								//System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 							}
 						}
 
@@ -387,12 +285,12 @@ public class processes
 
 							currentBurstT = burstTimeForP(burstSortedT, process, processCount);
 							printSelected(bw, t, process, currentBurstT);	//DEBUG
-							System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+							//System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 							//System.out.println("Burst:"+Arrays.deepToString(burstSortedT));			//DEBUG
 							t = prevSelectT+currentBurstT;	//account for t++ (-1), on firstR we dont want this
 							prevSelectP = process;
 							prevSelectT = t;
-							System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+							//System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 						}
 
 					}
@@ -404,13 +302,13 @@ public class processes
 						if(process != 0)
 						{
 							if( !isPFinished(finished, process, processCount) )
-							{	System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+							{	//System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 								currentBurstT = burstTimeForP(burstSortedT, process, processCount);
 								printFinished(bw, prevSelectT+currentBurstT - 1, process);
 								insertFinishedData(finished, process, t+currentBurstT, processCount);
 								updateBTimeToZero(burstSortedT, process, processCount);
 								t = prevSelectT+currentBurstT;  //t++ is why -1
-								System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+								//System.out.println("Finished bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 							}
 						}
 
@@ -427,12 +325,12 @@ public class processes
 
 							currentBurstT = burstTimeForP(burstSortedT, process, processCount);
 							printSelected(bw, t - 1, process, currentBurstT);	//DEBUG
-							System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
+							//System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG
 							//System.out.println("Burst:"+Arrays.deepToString(burstSortedT));			//DEBUG
 							//t = prevSelectT+currentBurstT;	//account for t++ (-1), on firstR we dont want this
 							prevSelectP = process;
 							prevSelectT = t;
-							System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG	
+							//System.out.println("Select bt_"+currentBurstT+" t_"+t+" P_"+process);	//DEBUG	
 						}			
 					}
 
@@ -443,9 +341,12 @@ public class processes
 					//System.out.println("End gb_"+greatestBurst+" t_"+t+" P_"+process);	//DEBUG
 					t++;
 					firstRun = 0;
+					lastKnownT = t;
 
 				}//END When last process running didn't finish while
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+				t = finished[0].length;
+				t = finished[1][t-1] - 2;	//there is a descrepency of 2 somewhere
 
 				while(t < runFor )
 				{
@@ -461,9 +362,9 @@ public class processes
 
 				//finished[1][p] - arivedSortedT[1][p] - arivalBurst[p+1][1];
 				timeThisPArrived(arivedSortedT,process,processCount);
-				System.out.println("Finished:"+Arrays.deepToString(finished));	//DEBUG
-				System.out.println("Arrived:"+Arrays.deepToString(arivedSortedT));	//DEBUG
-				System.out.println("arivalBurst:"+Arrays.deepToString(arivalBurst));	//DEBUG
+				//System.out.println("Finished:"+Arrays.deepToString(finished));	//DEBUG
+				//System.out.println("Arrived:"+Arrays.deepToString(arivedSortedT));	//DEBUG
+				//System.out.println("arivalBurst:"+Arrays.deepToString(arivalBurst));	//DEBUG
 
 
 				for(int i = 0; i < arivedSortedT[0].length; i++)
@@ -478,9 +379,6 @@ public class processes
 				}	
 
 			}//END previous selected didn't finish
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 			//////////////////////////////////////////////////////
 			// 	End output to file
@@ -499,7 +397,7 @@ public class processes
 
 	public static boolean weFinishedPrevSelectedP(Integer[][] finished, int processCount, int prevSelectP)
 	{
-		System.out.println("preSelected_"+prevSelectP);	//DEBUG
+		//System.out.println("preSelected_"+prevSelectP);	//DEBUG
 
 		for(int i = 0; i < processCount; i++)
 		{
